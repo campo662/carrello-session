5878d77088dab1654d9e40438422111516d7eac9
AuraJacket7 <trizzino03@gmail.com>
2025-11-18 14:13:21 +0100

    Update check_login.php

--- check_login.php
+++ check_login.php
@@ -1,11 +1,30 @@
-<!DOCTYPE html>
-<html lang="it">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Informazioni</title>
-    </head>
-    <body>
-        <h3>Informazioni</h3>
-    </body>
-</html>
+<?php
+	if (isset($_SESSION['loggato']) && $_SESSION['loggato'] === true) {
+			// Reindirizza l'utente alla pagina index
+			header("Location: index.php");
+			exit;
+		}
+		$errore_login = "";
+
+		// Quando viene inviato il form
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+
+			// Verifica delle credenziali 
+			if ($username === $username_valido && $password === $password_valida) {
+				
+				// Login corretto: Imposta le variabili di sessione
+				$_SESSION['loggato'] = true; // Flag per lo stato di accesso
+				$_SESSION['username'] = $username;
+
+				// vai pagina index
+				header("Location: index.php"); // CAMBIA con il nome della tua pagina protetta
+				exit;
+			} else {
+				// Login fallito
+				$errore_login = "Username o password errati.";
+			}
+		}
+?>		
