524e91f6e801b8b4424af348fcda1837e887a8e4
campo662 <10687@dalcero.edu.it>
2025-11-19 22:28:51 +0100

    Update login page title and formatting

--- login.php
+++ login.php
@@ -22,7 +22,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width,initial-scale=1.0">
-    <title>LOGIN</title>
+    <title>Login al carrello</title>
 </head>
 <body>
 	<h3>Login</h3>
