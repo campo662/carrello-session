29d92a70fbc86a5dc8601f506b2509f9f0fdb3a2
campo662 <10687@dalcero.edu.it>
2025-11-20 17:48:12 +0100

    Refactor login logic and improve error handling

--- check_login.php
+++ check_login.php
@@ -1,31 +1,35 @@
 <?php
-	if (isset($_SESSION['controllo']) && $_SESSION['controllo'] === true) {
+	if (isset($_SESSION['loggato']) && $_SESSION['loggato'] === true) {
 			// Reindirizza l'utente alla pagina index
 			header("Location: index.php");
 			exit;
 		}
-		$errore_login = "";
+	
+	$user="simo";
+	$pass="vosso";
 
-		// Quando viene inviato il form
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	// Quando viene inviato il form
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			
-			$username = $_POST['username'];
-			$password = $_POST['password'];
+		$username = $_POST['username'];
+		$password = $_POST['password'];
 
-			// Verifica delle credenziali 
-			if ($username === $username_valido && $password === $password_valida) {
+		// Verifica delle credenziali 
+		if ($username === $user && $password === $pass) {
 				
-				// Login corretto: Imposta le variabili di sessione
-				$_SESSION['controllo'] = true; // Flag per lo stato di accesso
-				$_SESSION['username'] = $username;
-
-				// vai pagina index
-				header("Location: index.php"); // CAMBIA con il nome della tua pagina protetta
-				exit;
-			} else {
-				// Login fallito
-				$errore_login = "Username o password errati.";
-			}
+			// Login corretto: Imposta le variabili di sessione
+			$_SESSION['loggato'] = true; // Flag per lo stato di accesso
+			$_SESSION['username'] = $username;
+			header("Location: index.php"); // CAMBIA con il nome della tua pagina protetta
+			exit;
+		} 
+		else {
+			// Login fallito
+			echo"<h3>Login Fallito</h3><hr>";
+			echo"<p>Username o password errati.</p>";
+			$errore_login = "Username o password errati.";
+			echo "<script>alert('$errore_login')</script>";
+			echo"<br><button><a href=login.php>Torna al login</a></button>";
 		}
+	}
 ?>		
-
