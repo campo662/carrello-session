7d1783cca979e5bf4c4b2708e2b034957b96e3b7
campo662 <10687@dalcero.edu.it>
2025-11-20 23:56:28 +0100

    Refactor login.php to include CSS and clean up code
    Updated the login page to include a CSS stylesheet and removed unnecessary comments.

--- login.php
+++ login.php
@@ -19,79 +19,26 @@
 ?>
 <!DOCTYPE html>
 <html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <meta content="width=device-width,initial-scale=1.0">
-    <title>Login al carrello</title>
-	<style>
-		body {
-			font-family: Arial, sans-serif;
-			background-color: #f2f2f2; /* colore piatto */
-			margin: 0;
-			padding: 20px;
-			line-height: 1.5;
-			text-align: center; /* tutto centrato */
-		}
-
-		
-		 h3{
-			color: #333;
-			margin-bottom: 20px;
-		}
-
-		
-		label {
-			font-size: 14px;
-			display: block;
-			margin-top: 10px;
-		}
-
-		
-		div {
-			margin-bottom: 10px;
-		}
-
-		
-		hr {
-			margin: 20px auto;
-			border: 0;
-			height: 1px;
-			background-color: #ccc;
-		}
-
-		
-		button {
-			padding: 8px 16px;
-			margin-top: 10px;
-			background-color: #007bff;
-			color: white;
-			border: none;
-			border-radius: 5px;
-			cursor: pointer;
-			font-size: 14px;
-			display: inline-block;
-		}
-
-		button:hover {
-			background-color: #0056b3;
-		}
-
-	</style>
-    
-</head>
-<body>
-	<h3>Login</h3>
-	<form action = "check_login.php" method = "POST">
-		<div>	
-			<label for ="username">Username:</label>
-			<input type ="text" id ="username" name ="username" required>
-		</div>
-		<div>	
-			<label for ="password">Password:</label>
-			<input type ="password" id ="password" name ="password" required>
-		</div>
-		<br>
-		<button type="submit">Login</button>
-	</form>	
-</body>
+	<head>
+	    <meta charset="UTF-8">
+	    <meta content="width=device-width,initial-scale=1.0">
+		<link rel="stylesheet" href="css.css">
+	    <title>Login al carrello</title>
+	</head>
+	<body>
+		<h3>Login</h3>
+		<form action = "check_login.php" method = "POST">
+			<div>	
+				<label for ="username">Username:</label>
+				<input type ="text" id ="username" name ="username" required>
+			</div>
+			<div>	
+				<label for ="password">Password:</label>
+				<input type ="password" id ="password" name ="password" required>
+			</div>
+			<br>
+			<button type="submit">Login</button>
+		</form>	
+	</body>
 </html>	
+
