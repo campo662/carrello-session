598962b6133738f81eb88ea0440bcf60ad4aef8b
campo662 <10687@dalcero.edu.it>
2025-11-21 13:19:09 +0100

    Update carrello.php

--- carrello.php
+++ carrello.php
@@ -6,6 +6,30 @@
         header("Location: login.php");
         exit();
     }
+
+    // gestione bottoni
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+        if(isset($_POST["aggiungi"])) {
+            $prodotto = $_POST["aggiungi"];
+            if(isset($_SESSION["carrello"][$prodotto])) {
+                $_SESSION["carrello"][$prodotto] = $_SESSION["carrello"][$prodotto] + 1;
+            } 
+            else {
+                $_SESSION["carrello"][$prodotto] = 1;
+            }
+        }
+
+        if(isset($_POST["rimuovi"])) {
+            $prodotto = $_POST["rimuovi"];
+            if(isset($_SESSION["carrello"][$prodotto])){
+                $_SESSION["carrello"][$prodotto]--;
+                if($_SESSION["carrello"][$prodotto] <= 0){
+                    unset($_SESSION["carrello"][$prodotto]);
+                }
+            }
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -17,17 +41,35 @@
         <title>Carrello della spesa - riepilogo</title>
     </head>
     <body>
+        <div id="titolo">
+            <h2>Gestione carrello</h2>
+        </div>
+        <hr>
+
         <h3>Riepilogo carrello</h3>
         <?php
         if(empty($_SESSION["carrello"])){
             echo "Il carrello è vuoto.";
-        } else {
+        } 
+        else {
             $prezzo_totale = 0;
             foreach($_SESSION["carrello"] as $prodotto => $quantita){
                 $prezzo = $_SESSION["lista_prodotti"][$prodotto];
                 $prezzo_totale_prodotto = $prezzo* $quantita;
                 $prezzo_totale = $prezzo_totale + $prezzo_totale_prodotto;
-                echo "$prodotto - $prezzo € x $quantita = $prezzo_totale_prodotto €<br>";
+                echo "$prodotto - $prezzo € x $quantita = $prezzo_totale_prodotto €"; 
+                ?>
+
+                <form method="POST" style="display:inline;">
+                    <button name="aggiungi" value="<?= $prodotto ?>">+</button>
+                </form>
+
+                <form method="POST" style="display:inline;">
+                    <button name="rimuovi" value="<?= $prodotto ?>">-</button>
+                </form>
+                <br>
+
+                <?php
             }
             echo "<br><h4>Prezzo totale: $prezzo_totale €</h4>";
         }
@@ -37,18 +79,22 @@
         <hr>
         
         <h3>Possibili azioni</h3>
-        <form method="POST" action="index.php">
+        <form method="POST" action="index.php" style="display:inline;">
             <button type="submit">Torna al carrello</button>
         </form>
-        <br>
-        <form method="POST" action="logout.php">
+        <form method="POST" action="logout.php" style="display:inline;">
             <button type="submit">Effettua il logout</button>
         </form>
-        <br>
-        <form method="POST" action="svuota.php">
+        <form method="POST" action="svuota.php" style="display:inline;">
             <button type="submit">Svuota il carrello</button>
         </form>
+
+        <div class="esempio">
+            A cura di:
+            <br>
+            Tommaso Camponogara
+            <br>
+            Simone Trizzino
+        </div>
     </body>
 </html>
-
-
